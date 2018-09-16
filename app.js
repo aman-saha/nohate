572b78f1fbcb7c782516757dd70b756d1c006033
aman-saha <aman.saha2015@vit.ac.in>
2018-09-16 14:25:57 +0530

    working falsk and nodejs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var mustacheExpress = require('mustache-express');
 var io = require('socket.io')(http);
-let {PythonShell} = require('python-shell')
+var request = require('request');
 const fs = require('fs');
 var csvWriter = require('csv-write-stream')
 const body_parser = require('body-parser');
@@ -29,39 +29,21 @@ io.on('connection', function(socket){
     writer.pipe(fs.createWriteStream('backend/out.csv'));
     writer.write({id: 1, comment_text: msg});
     writer.end();
-    PythonShell.run('backend/clf.py', null, function (err) {
-      if (err) throw err;
-      console.log('finished');
-    });
     console.log('message is :' + msg);
+    request.post(
+      'http://127.0.0.1:5000/classify',
+      function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+              console.log(body)
+          }
+      }
+  );
     io.sockets.emit('chat message',msg);
   });
   socket.on('disconnect', function(){
     console.log('user disconnected');
   }); 
 });
-function callName(req, res) { 
-      
-  // Use child_process.spawn method from  
-  // child_process module and assign it 
-  // to variable spawn 
-  var spawn = require("child_process").spawn; 
-    
-  // Parameters passed in spawn - 
-  // 1. type_of_script 
-  // 2. list containing Path of the script 
-  //    and arguments for the script  
-    
-  // E.g : http://localhost:3000/name?firstname=Mike&lastname=Will 
-  // so, first name = Mike and last name = Will 
-  var process = spawn('python',["./backend/clf.py"]); 
-
-  // Takes stdout data from script which executed 
-  // with arguments and send this data to res object 
-  process.stdout.on('data', function(data) { 
-      res.send(data.toString()); 
-  } ) 
-} 
 /*app.post('/send_message', (req,res) => {
   res.json({
     message:req.body.message
--- backend/clf.py
+++ backend/clf.py
@@ -1,50 +1,71 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# coding: utf-8
+from flask import Flask
+app = Flask(__name__)
+
 import pandas as pd, numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import re, string
 import csv
+import operator
 
-train = pd.read_csv('train.csv')
-test = pd.read_csv('out.csv')
+@app.route("/classify",methods=['POST'])
+def hello():
+    #return "Hello World from Python!"
+    train = pd.read_csv('data/train.csv')
+    test = pd.read_csv('out.csv')
 
-label_cols = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
-train['none'] = 1-train[label_cols].max(axis=1)
+    label_cols = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+    train['none'] = 1-train[label_cols].max(axis=1)
 
-COMMENT = 'comment_text'
-train[COMMENT].fillna("unknown", inplace=True)
-test[COMMENT].fillna("unknown", inplace=True)
+    COMMENT = 'comment_text'
+    train[COMMENT].fillna("unknown", inplace=True)
+    test[COMMENT].fillna("unknown", inplace=True)
 
-re_tok = re.compile('([{string.punctuation}“”¨«»®´·º½¾¿¡§£₤‘’])')
-def tokenize(s): 
-	return re_tok.sub(r' \1 ', s).split()
+    re_tok = re.compile('([{string.punctuation}“”¨«»®´·º½¾¿¡§£₤‘’])')
+    def tokenize(s): 
+        return re_tok.sub(r' \1 ', s).split()
 
-n = train.shape[0]
-vec = TfidfVectorizer(ngram_range=(1,2), tokenizer=tokenize,
-               min_df=3, max_df=0.9, strip_accents='unicode', use_idf=1,
-               smooth_idf=1, sublinear_tf=1 )
-trn_term_doc = vec.fit_transform(train[COMMENT])
-test_term_doc = vec.transform(test[COMMENT])
+    n = train.shape[0]
+    vec = TfidfVectorizer(ngram_range=(1,2), tokenizer=tokenize,
+                min_df=3, max_df=0.9, strip_accents='unicode', use_idf=1,
+                smooth_idf=1, sublinear_tf=1 )
+    trn_term_doc = vec.fit_transform(train[COMMENT])
+    test_term_doc = vec.transform(test[COMMENT])
 
-x = trn_term_doc
-test_x = test_term_doc
+    x = trn_term_doc
+    test_x = test_term_doc
 
-def pr(y_i, y):
-    p = x[y==y_i].sum(0)
-    return (p+1) / ((y==y_i).sum()+1)
+    def pr(y_i, y):
+        p = x[y==y_i].sum(0)
+        return (p+1) / ((y==y_i).sum()+1)
 
-def get_mdl(y):
-    y = y.values
-    r = np.log(pr(1,y) / pr(0,y))
-    m = LogisticRegression(C=4, dual=True)
-    x_nb = x.multiply(r)
-    return m.fit(x_nb, y), r
+    def get_mdl(y):
+        y = y.values
+        r = np.log(pr(1,y) / pr(0,y))
+        m = LogisticRegression(C=4, dual=True)
+        x_nb = x.multiply(r)
+        return m.fit(x_nb, y), r
 
-preds = np.zeros((len(test), len(label_cols)))
+    preds = np.zeros((len(test), len(label_cols)))
 
-for i, j in enumerate(label_cols):
-    print('fit', j)
-    m,r = get_mdl(train[j])
-    preds[:,i] = m.predict_proba(test_x.multiply(r))[:,1]
+    for i, j in enumerate(label_cols):
+        print('fit', j)
+        m,r = get_mdl(train[j])
+        preds[:,i] = m.predict_proba(test_x.multiply(r))[:,1]
 
-print (preds[0])
+    print (preds[0])
+    xhatex = {
+        "toxic" : preds[0][0],
+        "severe_toxic" : preds[0][1],
+        "obscene" : preds[0][2],
+        "threat" : preds[0][3],
+        "insult" : preds[0][4],
+        "identity_hate" : preds[0][5]
+    }
+    res = max(xhatex.items(), key=operator.itemgetter(1))[0]
+    if(xhatex[res]<0.40):
+        return ('hatespeech')
+    else:
+        return ('allow speech')
